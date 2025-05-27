0cf030e05838b39c60ad91ce97e7ef15667ed8e3
hazbola <hckim@spiderlab.co.kr>
2025-05-27 15:27:29 +0900

    111

--- bookinfo/api.py
+++ bookinfo/api.py
@@ -82,7 +82,7 @@ async def scrape_book_get(kyobo_id: str):
 
 if __name__ == "__main__":
     uvicorn.run(
-        "start:app",
+        "api:app",
         host="0.0.0.0",
         port=8000,
         reload=True,
