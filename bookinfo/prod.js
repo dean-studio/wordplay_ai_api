3479befb41b28cd7f9435415ffbc9994c8d0ad00
hazbola <hckim@spiderlab.co.kr>
2025-06-12 13:20:45 +0900

    111

--- bookinfo/prod.js
+++ bookinfo/prod.js
@@ -2,11 +2,16 @@ module.exports = {
   apps: [
     {
       name: "kyobo-api",
-      script: "/home/dean/fastapi/venv/bin/python",
+      script: "python",
       args: "api.py",
       instances: 1,
       autorestart: true,
       watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+        PATH: "/home/dean/fastapi/venv/bin:" + process.env.PATH
+      },
       error_file: "./logs/err.log",
       out_file: "./logs/out.log",
       log_file: "./logs/combined.log",
