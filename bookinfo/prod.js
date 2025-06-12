4b0fb761135439a54179a96eb4dfd3b2a018f80e
hazbola <hckim@spiderlab.co.kr>
2025-06-12 13:19:09 +0900

    111

--- bookinfo/prod.js
+++ bookinfo/prod.js
@@ -7,11 +7,6 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: "1G",
-      env: {
-        NODE_ENV: "production",
-        PYTHONPATH: "."
-      },
       error_file: "./logs/err.log",
       out_file: "./logs/out.log",
       log_file: "./logs/combined.log",
